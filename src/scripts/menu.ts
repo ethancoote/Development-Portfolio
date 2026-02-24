b08aaf81c44718ceae452c0a1436ddad07f79051
ethancoote <realethancoote@gmail.com>
2026-02-23 20:18:47 -0500

    fixing mobile bug that wouldn't allow clicks on projects

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -7,30 +7,36 @@ const mobileMenu = document.querySelector("#mobile-menu") as HTMLElement;
 let menuOpen = false;
 menuBtnElem?.addEventListener("click", () => {
     if (menuOpen) {
-        menuOpen = false;
-        mobileMenu.style.opacity = "0%";
-        mobileMenu.style.pointerEvents = "none";
-        document.body.style.overflow = "auto"; 
-        burgerImg.src = "./images/open.svg";
+        closeMenu()
     } else {
         menuOpen = true;
+        mobileMenu.style.zIndex = "10";
         mobileMenu.style.opacity = "100%";
         mobileMenu.style.pointerEvents = "auto";
         document.body.style.overflow = "hidden";
+        
         burgerImg.src = "./images/close.svg";
     }
+
 });
 
 // close menu on button click
 const menuButtonArray = document.querySelectorAll(".mobile-menu-button");
 menuButtonArray?.forEach(btn => {
     btn.addEventListener("click", () => {
-        menuOpen = false;
-        mobileMenu.style.opacity = "0%";
-        mobileMenu.style.pointerEvents = "none";
-        document.body.style.overflow = "auto";
-        burgerImg.src = "./images/open.svg";
+        closeMenu();
     });
 });
+
+function closeMenu () {
+    menuOpen = false;
+    mobileMenu.style.opacity = "0%";
+    mobileMenu.style.pointerEvents = "none";
+    document.body.style.overflow = "auto"; 
+    burgerImg.src = "./images/open.svg";
+    setTimeout(() => {
+        mobileMenu.style.zIndex = "-1";
+    }, 250);
+}
 
 
